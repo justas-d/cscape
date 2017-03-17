e72d281ad092166587bcf4c409a94ea52a39ecb9
Justas Dabrila <justasdabrila@gmail.com>
2017-03-17 02:01:28 +0000

    Actually switched to EF6

--- cscape-netcore/IPlayerDatabase.cs
+++ cscape-netcore/IPlayerDatabase.cs
@@ -5,9 +5,10 @@ namespace cscape
     public interface IPlayerDatabase
     {
         Task<bool> UserExists(string username);
-        Task<IPlayerSaveData> Load(string username, string password);
-        Task Save(Player player);
-        Task<IPlayerSaveData> LoadOrCreateNew(string username, string pwd);
         Task<bool> IsValidPassword(string pwdHash, string pwd);
+
+        Task<IPlayerSaveData> Load(string username, string password);
+        Task<IPlayerSaveData> Save(Player player);
+        Task<IPlayerSaveData> LoadOrCreateNew(string username, string pwd);
     }
 }
