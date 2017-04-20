51ad8530081661aa2ac82d8a37a65972aca4c845
Justas Dabrila <justasdabrila@gmail.com>
2017-04-20 23:42:20 +0000

    Further notes on player updating

--- cscape-netcore/PlayerUpdateSyncMachine.cs
+++ cscape-netcore/PlayerUpdateSyncMachine.cs
@@ -18,11 +18,34 @@ namespace cscape
 
             // todo : implement all types of player updating
 
-            // for now, simple init-esque updating will do
+            // for now, simple init-spam updating will do
 
             // -- local
-            stream.WriteBits(1, 1); // should update
+            stream.WriteBits(1, 1); // continue reading? or just "does need updating at all"? If 0, no flag updates will be read for local.
+
+            /* Types of updates
+             * 
+             *  0 (just flag the player)
+             *      No further reading. Local player is queued for a flag update.
+             *      
+             *  1 (walk)
+             *      3 bits - movement direction
+             *      1 bit  - should queue flag update?
+             *      
+             *  2 (run)
+             *      3 bits - movement direction 1
+             *      3 bits - movement direction 2
+             *      1 bit  - should queue flag update?
+             *      
+             *  3 (init)
+             *      2 bits - z plane
+             *      1 bit  - should set flag to true when calling setPos?
+             *      1 bit  - should queue flag update?
+             *      7 bits - local y
+             *      7 bits - local x
+             */
             stream.WriteBits(2, 3); // type
+
             stream.WriteBits(2, Pos.Z); // plane
             stream.WriteBits(1, 1); // setPos flag
             stream.WriteBits(1, 1); // add to local list
