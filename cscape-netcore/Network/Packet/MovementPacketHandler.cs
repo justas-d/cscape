0af285dbb53fbc4e82db5894105dfc51e8496e30
Justas Dabrila <justasdabrila@gmail.com>
2017-04-27 04:30:45 +0000

    Movement packet handler initial

---
+++ cscape-netcore/Network/Packet/MovementPacketHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CScape.Data;
+using CScape.Game.Entity;
+
+namespace CScape.Network.Packet
+{
+    public sealed class MovementPacketHandler : IPacketHandler
+    {
+        public int[] Handles { get; } =
+        {
+            248, // map walk
+            98,
+            164,
+            36
+        };
+
+        public const int MaxTiles = 25;
+
+        public void Handle(Player player, int opcode, Blob packet)
+        {
+            var length = packet.Buffer.Length;
+
+        }
+    }
+}
